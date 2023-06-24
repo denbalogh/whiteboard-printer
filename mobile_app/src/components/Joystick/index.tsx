6ebd5cad5e9e102300ea43215aaa9e99d5cf4390
Denis Balogh <denbalogh@gmail.com>
2023-06-24 15:26:08 +0200

    mobile app - joystick

--- mobile_app/src/components/Joystick/index.tsx
+++ mobile_app/src/components/Joystick/index.tsx
@@ -1,26 +1,40 @@
 import React, {useRef} from 'react';
 import {Animated, PanResponder, StyleSheet, View} from 'react-native';
+import _ from 'lodash';
 
-const Joystick = () => {
+const Joystick = ({
+  onMove,
+  throttleMs = 300,
+}: {
+  onMove: ({x, y}: {x: number; y: number}) => void;
+  throttleMs?: number;
+}) => {
+  const throttleFn = _.throttle(onMove, throttleMs);
   const pan = useRef(new Animated.ValueXY()).current;
-
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: Animated.event(
-      [
-        null,
-        {
-          dx: pan.x, // x,y are Animated.Value
-          dy: pan.y,
-        },
-      ],
-      {useNativeDriver: false},
-    ),
+    onPanResponderMove: (e, gestureState) => {
+      Animated.event(
+        [
+          null,
+          {
+            dx: pan.x, // x,y are Animated.Value
+            dy: pan.y,
+          },
+        ],
+        {useNativeDriver: false},
+      )(e, gestureState);
+
+      throttleFn({x: gestureState.dx, y: gestureState.dy});
+    },
     onPanResponderRelease: () => {
       Animated.spring(
         pan, // Auto-multiplexed
         {toValue: {x: 0, y: 0}, useNativeDriver: true}, // Back to zero
       ).start();
+
+      throttleFn.cancel();
+      onMove({x: 0, y: 0});
     },
   });
 
@@ -29,7 +43,24 @@ const Joystick = () => {
       <Animated.View
         {...panResponder.panHandlers}
         style={[
-          {transform: [{translateX: pan.x}, {translateY: pan.y}]},
+          {
+            transform: [
+              {
+                translateX: pan.x.interpolate({
+                  inputRange: [-50, 0, 50],
+                  outputRange: [-50, 0, 50],
+                  extrapolate: 'clamp',
+                }),
+              },
+              {
+                translateY: pan.y.interpolate({
+                  inputRange: [-50, 0, 50],
+                  outputRange: [-50, 0, 50],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ],
+          },
           styles.box,
         ]}
       />
@@ -39,15 +70,18 @@ const Joystick = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: 150,
+    height: 150,
+    backgroundColor: 'lightgray',
+    borderRadius: 150,
     justifyContent: 'center',
     alignItems: 'center',
   },
   box: {
-    backgroundColor: '#61dafb',
+    backgroundColor: 'gray',
     width: 80,
     height: 80,
-    borderRadius: 4,
+    borderRadius: 50,
   },
 });
 
--- mobile_app/src/screens/Control/index.tsx
+++ mobile_app/src/screens/Control/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Box} from 'native-base';
+import {Center} from 'native-base';
 import Joystick from '../../components/Joystick';
-import useBluetoothContext from '../../contexts/bluetoothContext';
-import NotConnedtedToDevice from '../../components/NotConnectedToDevice';
+// import useBluetoothContext from '../../contexts/bluetoothContext';
+// import NotConnedtedToDevice from '../../components/NotConnectedToDevice';
 
 const ControlScreen = () => {
-  const {isConnected} = useBluetoothContext();
+  // const {isConnected} = useBluetoothContext();
 
-  if (!isConnected) {
-    return <NotConnedtedToDevice />;
-  }
+  // if (!isConnected) {
+  //   return <NotConnedtedToDevice />;
+  // }
+
+  const onMove = ({x, y}: {x: number; y: number}) => {
+    console.log({x, y});
+  };
 
   return (
-    <Box alignSelf="center">
-      <Joystick />
-    </Box>
+    <Center flex={1}>
+      <Joystick onMove={onMove} />
+    </Center>
   );
 };
 
